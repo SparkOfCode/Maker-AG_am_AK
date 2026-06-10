527f5aa565579a8452a23cda4a52210932a53478
SparkOfCode <wolfgang.henkel@gmx.de>
2026-06-11 00:08:13 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,6 +5,7 @@ namespace MakerAG {
      * Setze den Greifer von der aktuellen auf eine neue Position
      */
     export function Greifer (Position: number, Ziel: number) {
+
     while (Position < Ziel) {
         Position += 1
         calliBot2.servo(C2Servo.Servo1, Position)
