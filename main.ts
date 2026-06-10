6e96bca54f1c90426b68bf46072795dd8d6c8362
SparkOfCode <wolfgang.henkel@gmx.de>
2026-06-10 22:57:28 +0200

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,21 @@
-namespace MakerAG {
 
-    //%block
-
-    export function Greifer(Position2: number, Ziel: number) {
-        while (Position2 < Ziel) {
-            Position2 += 1
-            calliBot2.servo(C2Servo.Servo1, Position2)
-            basic.pause(30)
-        }
-        while (Position2 > Ziel) {
-            Position2 += -1
-            calliBot2.servo(C2Servo.Servo1, Position2)
-            basic.pause(30)
-        }
+/**
+ * Bietet Zugriff auf die grundlegende micro:bit Funktion.
+ */
+let Position = 0
+function Greifer(Position2: number, Ziel: number) {
+    while (Position2 < Ziel) {
+        Position2 += 1
+        calliBot2.servo(C2Servo.Servo1, Position2)
+        basic.pause(30)
+    }
+    while (Position2 > Ziel) {
+        Position2 += -1
+        calliBot2.servo(C2Servo.Servo1, Position2)
+        basic.pause(30)
     }
 }
+
 basic.forever(function () {
-	
+
 })
