0a95f0216b27c82c468d22097f5f4468f5ea6ff0
SparkOfCode <wolfgang.henkel@gmx.de>
2026-06-10 23:37:05 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,10 @@
 namespace MakerAG {
 
 //% block
-    export let Position2 = 2
-
-//% block
     /**
      * Displays a greeting for the player to read.
      */
-    export function Greifer (Ziel: number) {
+    export function Greifer (Position2: number, Ziel: number) {
     while (Position2 < Ziel) {
         Position2 += 1
         calliBot2.servo(C2Servo.Servo1, Position2)
