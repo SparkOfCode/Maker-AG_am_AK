b23d00cdf1d325cd3a978762f5712197e3fcf6a0
SparkOfCode <wolfgang.henkel@gmx.de>
2026-06-10 23:11:10 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,20 @@
 namespace MakerAG {
 
-//% block
-export function Greifer (Position2: number, Ziel: number) {
-    while (Position2 < Ziel) {
-        Position2 += 1
-        calliBot2.servo(C2Servo.Servo1, Position2)
-        basic.pause(30)
+    //% block
+    export let Position2 = 0
+    export function Greifer(Position2: number, Ziel: number) {
+        while (Position2 < Ziel) {
+            Position2 += 1
+            calliBot2.servo(C2Servo.Servo1, Position2)
+            basic.pause(30)
+        }
+        while (Position2 > Ziel) {
+            Position2 += -1
+            calliBot2.servo(C2Servo.Servo1, Position2)
+            basic.pause(30)
+        }
     }
-    while (Position2 > Ziel) {
-        Position2 += -1
-        calliBot2.servo(C2Servo.Servo1, Position2)
-        basic.pause(30)
-    }
-}
 }
 basic.forever(function () {
-	
+
 })
