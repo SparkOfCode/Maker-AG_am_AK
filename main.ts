883c620e1357e34ec691fa05c9d9285d2e9b577b
SparkOfCode <wolfgang.henkel@gmx.de>
2026-06-10 22:25:36 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,9 @@ namespace MakerAG {
 
     //%block
     export let Position2 = 120
-    export function Greifer(Ziel: number) {
+
+	//%block
+	export function Greifer(Ziel: number) {
         while (Position2 < Ziel) {
             Position2 += 1
             calliBot2.servo(C2Servo.Servo1, Position2)
