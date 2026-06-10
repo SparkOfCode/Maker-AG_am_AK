b9559873b15452642199b4403978e244c9ad4df4
SparkOfCode <wolfgang.henkel@gmx.de>
2026-06-10 22:39:43 +0200

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ namespace MakerAG {
 
     //%block
 
+    export let testvar = 0
     export function Greifer(Position2: number, Ziel: number) {
         while (Position2 < Ziel) {
             Position2 += 1
