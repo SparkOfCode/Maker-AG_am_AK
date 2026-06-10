c34c8ab352d117d8f8e0b2cfe586a27f4074e2a8
SparkOfCode <wolfgang.henkel@gmx.de>
2026-06-10 23:09:23 +0200

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
-let Position2 = 0
-function Greifer (Position2: number, Ziel: number) {
+namespace MakerAG {
+
+//% block
+export function Greifer (Position2: number, Ziel: number) {
     while (Position2 < Ziel) {
         Position2 += 1
         calliBot2.servo(C2Servo.Servo1, Position2)
@@ -10,6 +12,7 @@ function Greifer (Position2: number, Ziel: number) {
         calliBot2.servo(C2Servo.Servo1, Position2)
         basic.pause(30)
     }
+}
 }
 basic.forever(function () {
 	
